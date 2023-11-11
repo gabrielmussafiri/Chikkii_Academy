eb8b288529ee0225ca9f68c19d729d8f86530ad3
gabrielmussafiri <musgabriel2@gmail.com>
2023-11-11 10:19:27 +0200

    updated

--- chikkiiacademyapp/app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,7 +0,0 @@
-const SignInPage = () => {
-    return ( <div>
-        <p>This is Unprocted page</p>
-    </div> );
-}
- 
-export default SignInPage;
---
+++ chikkiiacademyapp/app/test/page.tsx
@@ -0,0 +1,9 @@
+const TestPage = () => {
+    return ( 
+        <div>
+            This should be Unprotected Page
+        </div>
+     );
+}
+ 
+export default TestPage;
